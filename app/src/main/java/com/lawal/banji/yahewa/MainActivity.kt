046a305402faaa990cbf1a17c76141c036434863
Banji Lawal <banji.k.lawal@gmail.com>
2025-03-30 15:24:45 -0500

    Added svg icons to res/draw. Updated name sent to MainActivity.Greetng()     to application name.

--- app/src/main/java/com/lawal/banji/yahewa/MainActivity.kt
+++ app/src/main/java/com/lawal/banji/yahewa/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             YahewaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Yahewa",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = name,
         modifier = modifier
     )
 }
