e35cdbc5a41cca991e416f4c0f8138b392188cbe
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-06 01:45:37 -0500

    Added Drawable for an icon representing the current conditions. added nmethod for processing drawables to WaetherObservationDisplay

--- app/src/main/java/com/lawal/banji/yahewa/WeatherObservation.kt
+++ app/src/main/java/com/lawal/banji/yahewa/WeatherObservation.kt
@@ -1,5 +1,8 @@
 package com.lawal.banji.yahewa
 
+import android.graphics.drawable.Drawable
+import android.media.Image
+
 data class WeatherObservation(
     val location: String,
     val currentTemperature: Temperature,
@@ -8,5 +11,6 @@ data class WeatherObservation(
     val lowTemperature: Temperature,
     val highTemperature: Temperature,
     val percentHumidity: Int,
-    val pressure: Int
+    val pressure: Int,
+    val icon: Drawable?
 )
--- app/src/main/java/com/lawal/banji/yahewa/WeatherObservationDisplay.kt
+++ app/src/main/java/com/lawal/banji/yahewa/WeatherObservationDisplay.kt
@@ -1,15 +1,43 @@
 package com.lawal.banji.yahewa
 
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
+
+@Composable
+fun WeatherIcon(icon: Drawable?) {
+    if (icon != null) {
+        Image(
+            bitmap = icon.toBitmap().asImageBitmap(),
+            contentDescription = "Weather Icon",
+            modifier = Modifier.size(24.dp)
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.farenheit),
+            contentDescription = "Default Weather Icon",
+            modifier = Modifier
+                .size(24.dp)
+                .background(Color.White)
+        )
+    }
+}
 
 @Composable
 fun WeatherObservationDisplay(weatherObservation: WeatherObservation) {
     Column(modifier = Modifier.padding(16.dp)) {
+        WeatherIcon(weatherObservation.icon)
         Text(text = "Location: ${weatherObservation.location}")
         Text(text = "Current Temperature: ${weatherObservation.currentTemperature}")
         Text(text = "Feels Like: ${weatherObservation.feelsLikeTemperature}")
