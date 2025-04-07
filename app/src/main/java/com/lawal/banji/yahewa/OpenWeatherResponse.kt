ee6406a6117e1772d1c7400c878a83c725e57f9a
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-06 20:27:12 -0500

    Added Main, OpenWeatherResponse, and Weather classes for the OpenWeather JSON response

---
+++ app/src/main/java/com/lawal/banji/yahewa/OpenWeatherResponse.kt
@@ -0,0 +1,9 @@
+package com.lawal.banji.yahewa
+
+data class OpenWeatherResponse(
+    val main: Main,
+    val weather: List<Weather>
+)
+
+
+
---
+++ app/src/main/java/com/lawal/banji/yahewa/Weather.kt
@@ -0,0 +1,6 @@
+package com.lawal.banji.yahewa
+
+data class Weather(
+    val description: String,
+    val icon: String
+)
