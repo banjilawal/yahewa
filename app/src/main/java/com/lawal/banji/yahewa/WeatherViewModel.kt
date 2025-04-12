aa3ef9bbdb8f563b8216d9fb4d40b8ee75b6fc72
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-12 00:38:32 -0500

    Wrote WeatherViewModel that gets the JSON response body.

--- app/src/main/java/com/lawal/banji/yahewa/WeatherViewModel.kt
+++ app/src/main/java/com/lawal/banji/yahewa/WeatherViewModel.kt
@@ -1,6 +1,28 @@
 package com.lawal.banji.yahewa
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lawal.banji.yahewa.RetrofitInstance.api
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
+    private val _weatherData = MutableLiveData<OpenWeatherResponse>()
+    val weatherData: LiveData<OpenWeatherResponse> get() = _weatherData
+
+    fun fetchWeatherData(latitude: Double, longitude: Double, apiKey: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = api.getWeatherData(latitude, longitude, apiKey = apiKey)
+                    .execute()
+                if (response.isSuccessful) {
+                    _weatherData.postValue(response.body())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
