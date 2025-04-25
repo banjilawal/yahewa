776b077b1ff2df4bab2a861a2a177da5481878a9
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-24 23:30:05 -0500

    Wrote TestScreen

--- app/src/main/java/com/lawal/banji/yahewa/destination/TestScreen.kt
+++ app/src/main/java/com/lawal/banji/yahewa/destination/TestScreen.kt
@@ -7,20 +7,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-
 import com.lawal.banji.yahewa.R
-
-import com.lawal.banji.yahewa.display.CurrentWeatherDisplay
 import com.lawal.banji.yahewa.ui.theme.Lavender
+import com.lawal.banji.yahewa.viewmodel.ForecastState
 import com.lawal.banji.yahewa.viewmodel.ForecastViewModel
 
 @Composable
 fun TestScreen(viewModel: ForecastViewModel) {
+
+    val forecastState = viewModel.forecastState.collectAsState().value
+
+
     Scaffold(
-        topBar = {},
+        topBar = {
+            Text(text = stringResource(id = R.string.app_name))
+        },
         bottomBar = {},
     ) { innerPadding ->
         Column(
@@ -29,16 +33,23 @@ fun TestScreen(viewModel: ForecastViewModel) {
                 .padding(innerPadding)
                 .background(Lavender)
         ) {
-            if (viewModel.forecast != null) {
-                CurrentWeatherDisplay(
-                    weatherRecord = weatherRecord,
-                    modifier = Modifier.fillMaxSize()
-                )
-            } else {
-                Text(
-                    text = stringResource(id = R.string.weather_data_error),
-                    modifier = Modifier.fillMaxSize()
-                )
+            when (forecastState) {
+                is ForecastState.Loading -> {
+                    Text(text = "Loading forecast...")
+                }
+                is ForecastState.Error -> {
+                    val errorMessage = (forecastState as ForecastState.Error).message
+                    Text(text = errorMessage)
+                }
+                is ForecastState.Success -> {
+                    val forecast = forecastState.forecast
+                    Text(text = forecast.city)
+                    Text(text = "Temperature ${forecast.main.temperature}")
+                    Text(text = "Low ${forecast.main.lowTemperature}")
+                    Text(text = "High  ${forecast.main.highTemperature}")
+                    Text(text = "Humidity ${forecast.main.percentHumidity}")
+                    Text(text = "Pressure ${forecast.main.pressure}")
+                }
             }
         }
     }
