db2a729f4940275657926ff7719079014f2edfb1
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-25 12:11:09 -0500

    Added more typography themes in ui.Type.kt

--- app/src/main/java/com/lawal/banji/yahewa/display/CurrentForecastView.kt
+++ app/src/main/java/com/lawal/banji/yahewa/display/CurrentForecastView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,13 +21,13 @@ fun CurrentForecastView(forecast: Forecast) {
             .fillMaxSize()
             .background(Lavender)
     ) {
-        item { Text(text = forecast.city) }
-        item { Text(text = "Icon ID: ${forecast.weather[0].iconId}") }
-        item { Text(text = "Weather: ${forecast.weather[0].description}") }
-        item { Text(text = "Temperature: ${forecast.main.temperature}°C") }
-        item { Text(text = "Low: ${forecast.main.lowTemperature}°C") }
-        item { Text(text = "High: ${forecast.main.highTemperature}°C") }
-        item { Text(text = "Humidity: ${forecast.main.percentHumidity}%") }
-        item { Text(text = "Pressure: ${forecast.main.pressure} hPa") }
+        item { Text(text = forecast.city, style = MaterialTheme.typography.bodyLarge) }
+        item { Text(text = "Icon ID: ${forecast.weather[0].iconId}", style = MaterialTheme.typography.bodyLarge)  }
+        item { Text(text = "Weather: ${forecast.weather[0].description}", style = MaterialTheme.typography.bodyLarge)  }
+        item { Text(text = "Temperature: ${forecast.main.temperature}°C", style = MaterialTheme.typography.bodyLarge)  }
+        item { Text(text = "Low: ${forecast.main.lowTemperature}°C", style = MaterialTheme.typography.bodyLarge) }
+        item { Text(text = "High: ${forecast.main.highTemperature}°C", style = MaterialTheme.typography.bodyLarge)  }
+        item { Text(text = "Humidity: ${forecast.main.percentHumidity}%", style = MaterialTheme.typography.bodyLarge)  }
+        item { Text(text = "Pressure: ${forecast.main.pressure} hPa", style = MaterialTheme.typography.bodyLarge) }
     }
 }
