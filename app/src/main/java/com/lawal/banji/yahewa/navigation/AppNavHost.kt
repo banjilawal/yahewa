333c6af8aab263f2dd398967bc38d026d9947b54
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-27 18:04:54 -0500

    Updated AppNavHost to suport using a button to swith between HoneScreen and Details. Added dynamic rute Details to Screens.

--- app/src/main/java/com/lawal/banji/yahewa/navigation/AppNavHost.kt
+++ app/src/main/java/com/lawal/banji/yahewa/navigation/AppNavHost.kt
@@ -3,8 +3,11 @@ package com.lawal.banji.yahewa.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.lawal.banji.yahewa.destination.DetailsScreen
 import com.lawal.banji.yahewa.destination.HomeScreen
 import com.lawal.banji.yahewa.view.model.ForecastViewModel
 
@@ -21,10 +24,24 @@ fun AppNavHost(
         composable(Screens.Home.route) {
             HomeScreen(
                 forecastState = forecastViewModel.forecastState.collectAsState().value,
+                onNavigate = { itemId -> navController.navigate(Screens.Details.createRoute(itemId.toString())) },
+                onZipcodeEntered = { zipcode -> forecastViewModel.setZipcode(zipcode) }
+            )
+        }
+
+        composable(
+            route = Screens.Details.route,
+            arguments = listOf(
+                navArgument("itemId") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            // Retrieve the itemId from the backStackEntry arguments
+            val itemId = backStackEntry.arguments?.getString("itemId")
+
+            DetailsScreen(
+                forecastState = forecastViewModel.forecastState.collectAsState().value,
                 onNavigate = {},
-                onZipcodeEntered = { zipcode ->
-                    forecastViewModel.setZipcode(zipcode)
-                }
+                onZipcodeEntered = { zipcode -> forecastViewModel.setZipcode(zipcode) }
             )
         }
     }
--- app/src/main/java/com/lawal/banji/yahewa/navigation/Screens.kt
+++ app/src/main/java/com/lawal/banji/yahewa/navigation/Screens.kt
@@ -2,8 +2,7 @@ package com.lawal.banji.yahewa.navigation
 
 sealed class Screens(val route: String) {
     object Home : Screens("home")
-    object Hourly : Screens("hourly")
-    object Details : Screens("details/{itemId}") {
+    object Details : Screens("details") {
         fun createRoute(itemId: String) = "details/$itemId"
     }
 }
