443727a04095201008c48b274192831c1872eb7c
Banji Lawal <banji.k.lawal@gmail.com>
2025-04-27 16:24:32 -0500

    Added model.FeelsLike data class.

--- app/src/main/java/com/lawal/banji/yahewa/model/daily/FeelsLike.kt
+++ app/src/main/java/com/lawal/banji/yahewa/model/daily/FeelsLike.kt
@@ -3,8 +3,8 @@ package com.lawal.banji.yahewa.model.daily
 import com.google.gson.annotations.SerializedName
 
 data class FeelsLike(
-    @SerializedName("temp") val current: Double,
-    @SerializedName("feels_like") val feelsLike: Double,
-    @SerializedName("temp_min") val min: Double,
-    @SerializedName("temp_max") val max: Double
+    @SerializedName("day") val day: Double,
+    @SerializedName("night") val night: Double,
+    @SerializedName("eve") val evening: Double,
+    @SerializedName("morn") val morn: Double
 )
