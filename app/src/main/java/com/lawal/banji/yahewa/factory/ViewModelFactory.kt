917618b583cbbcf44ce93ec7666109a098ba2ac1
Banji Lawal <banji.k.lawal@gmail.com>
2025-05-25 21:27:02 -0500

    Added CurrentWeatherViewModelFactory and ForecastViewMddelFactory inside ViewModelFactory.kt.

--- app/src/main/java/com/lawal/banji/yahewa/factory/ViewModelFactory.kt
+++ app/src/main/java/com/lawal/banji/yahewa/factory/ViewModelFactory.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.lawal.banji.yahewa.repo.AppRepository
 import com.lawal.banji.yahewa.request.PermissionHandler
 import com.lawal.banji.yahewa.view.model.AppViewModel
+import com.lawal.banji.yahewa.view.model.CurrentWeatherViewModel
+import com.lawal.banji.yahewa.view.model.ForecastViewModel
 import com.lawal.banji.yahewa.view.model.PermissionHandlerViewModel
 
 class WeatherViewModelFactory(
@@ -18,6 +20,34 @@ class WeatherViewModelFactory(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AppViewModel::class.java)) {
             return AppViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class CurrentWeatherViewModelFactory(
+    private val repository: AppRepository
+) : ViewModelProvider.Factory {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CurrentWeatherViewModel::class.java)) {
+            return CurrentWeatherViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class ForecastViewModelFactory(
+    private val repository: AppRepository
+) : ViewModelProvider.Factory {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ForecastViewModel::class.java)) {
+            return ForecastViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
